f614d429efef52ef5c82dbe4e692f4c134ff8138
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-04-02 08:59:35 -0500

    Removed comment / Ped Not Networked
    Removed the big comment of code just for organization purposes it just confuses my brain lol. Also changed the ped spawn from being networked to not networked. As when networked it will spawn a ped everytime someone joins the server.

--- client/shopped.lua
+++ client/shopped.lua
@@ -24,7 +24,7 @@ Citizen.CreateThread(function()
             end
         end
 
-        npc = CreatePed(model, v.Pos.x, v.Pos.y, v.Pos.z - 1.0, v.Pos.h, true, false, true, true)
+        npc = CreatePed(model, v.Pos.x, v.Pos.y, v.Pos.z - 1.0, v.Pos.h, false, false, true, true)
         Citizen.InvokeNative(0x283978A15512B2FE, npc, true)
         SetEntityCanBeDamaged(npc, false)
         SetEntityInvincible(npc, true)
@@ -53,27 +53,6 @@ Citizen.CreateThread(function()
     end
 end)
 
-
------------------Handles spawning the ped---------------------------
---[[Citizen.CreateThread(function()
-    local model = GetHashKey('mp_re_slumpedhunter_males_01') --sets the npc model
-    local v = Config.shop
-
-    RequestModel(model)               -- requests the varible model
-    if not HasModelLoaded(model) then --checks if its loaded
-        RequestModel(model)
-    end
-    while not HasModelLoaded(model) or HasModelLoaded(model) == 0 or model == 1 do -- not sure but its needed
-        Citizen.Wait(1)
-    end
-    local createdped = CreatePed(model, v.shoplocation.x, v.shoplocation.y, v.shoplocation.z - 1, false, true, true, true) --creates ped the minus one makes it so its standing on the ground not floating
-    Citizen.InvokeNative(0x283978A15512B2FE, createdped, true)                                                             -- sets ped into random outfit, stops it being invis
-    SetEntityAsMissionEntity(createdped, true, true)                                                                       -- sets ped as mission entity preventing it from despawning
-    SetEntityInvincible(createdped, true)                                                                                  --sets ped invincible
-    FreezeEntityPosition(createdped, true)                                                                                 --freezes the entity
-    ---------------------------Handles opening the menu
-
-end)]]
 --Creates the ability to use DrawText3D
 function DrawText3D(x, y, z, text)
     local onScreen, _x, _y = GetScreenCoordFromWorldCoord(x, y, z)
