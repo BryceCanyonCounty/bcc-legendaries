8f20855c0871aa3fd4f0e1befc2988d1746321e2
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-07-26 00:57:19 -0500

    bugFix+Optimizing

--- server.lua
+++ server.lua
@@ -11,8 +11,7 @@ TriggerEvent('bcc:getUtils', function(bccutils)
 end)
 
 ------------------------ Handles Giving Player Items when hunt over -----------------------
-RegisterServerEvent('bcc:legendaries:giveitemsbear')
-AddEventHandler('bcc:legendaries:giveitemsbear', function(Rewards)
+RegisterServerEvent('bcc:legendaries:giveitemsbear', function(Rewards)
   local _source = source
   for k, v in pairs(Rewards) do
     VorpInv.addItem(_source, v.name, v.count)
@@ -46,51 +45,23 @@ RegisterServerEvent('bcc:legendaries:menuopen5', function(Cost, shopid, cdownt)
     TriggerClientEvent('bcc:legendaries:menuopen4', _source)    --Robbery is not on cooldown
   end
 end)
----------------------------------------- TESTING DATABASE --------------------------------------------------
-
---------- This will create the bcc:legendaries table on script launch if it does not already exist -----------------
--------Pulled from bcc:pets and modified ----------------------
-CreateThread(function()
-  if Config.LevelSystem then
-    exports.oxmysql:execute([[CREATE TABLE if NOT EXISTS `legendaries` (
-      `identifier` varchar(50) NOT NULL, /*Creates a tab in the table for ident*/
-      `charidentifier` int(11) NOT NULL,
-      `trust` int(100) NOT NULL DEFAULT 0,
-      UNIQUE KEY `charidentifier` (`charidentifier`))
-    ]])
-  end
-end)
 
 --------- Checks if you exist in the DB, and if you do not it adds you to the DB aswell as adds 1 to your trust level----------------
-RegisterServerEvent('bcc:legendaries:DBCheck')
-AddEventHandler('bcc:legendaries:DBCheck', function(name)
+RegisterServerEvent('bcc:legendaries:DBCheck', function(name)
   local _source = source
   local Character = VORPcore.getUser(_source).getUsedCharacter
   local param = { ['charidentifier'] = Character.charIdentifier  , ['identifier'] = Character.identifier  }
   --------The if you exist in db code was pulled from vorp_banking and modified ----------------
-  exports.oxmysql:execute("SELECT identifier, charidentifier FROM legendaries WHERE identifier = @Playeridentifier AND charidentifier = @CharIdentifier", { ["@Playeridentifier"] = Character.identifier, ["CharIdentifier"] = Character.charIdentifier }, function(result)
-    if result[1] then
-      --Player already exists do nothing
-    else
-      exports.oxmysql:execute("INSERT INTO legendaries ( `charidentifier`,`identifier` ) VALUES ( @charidentifier,@identifier )", param)
+  local result = MySQL.query.await("SELECT identifier, charidentifier FROM legendaries WHERE identifier = @identifier AND charidentifier = @charidentifier", param)
+  if #result <= 0 then
+    MySQL.query.await("INSERT INTO legendaries ( `charidentifier`,`identifier` ) VALUES ( @charidentifier,@identifier )", param)
+  end
+  local result2 = MySQL.query.await("SELECT trust FROM legendaries WHERE charidentifier=@charidentifier AND identifier=@identifier", param)
+  if #result2 > 0 then
+    if result2[1].trust then
+      TriggerClientEvent('bcc-legendaries:ClientLevelCatch', _source, result2[1].trust, name) --passes trust to client
     end
-  end)
-  Wait(200)
-  local rep = false
-  repeat
-    local ran = false
-    exports.oxmysql:execute("SELECT trust FROM legendaries WHERE charidentifier=@charidentifier AND identifier=@identifier", param, function(result)
-      if result[1].trust then
-        rep = true
-        TriggerClientEvent('bcc-legendaries:ClientLevelCatch', _source, result[1].trust, name) --passes trust to client
-      end
-      ran = true
-    end)
-    while true do
-      Wait(10)
-      if ran then break end
-    end
-  until rep
+  end
 end)
 
 --This will handle version checking
