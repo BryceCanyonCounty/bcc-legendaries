fcf22068d1489d2565e0839323ab13b5da2b5d7b
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-01-16 16:41:57 -0800

    Update fxmanifest.lua
    - update fx version
    - update file structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,39 +1,37 @@
-fx_version "adamant"
-games {"rdr3"}
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+game 'rdr3'
 lua54 'yes'
-author 'Jake2k4'
+author 'Jake2k4 and BCC Team'
 
 shared_scripts {
-    'config.lua',
+    'configs/*.lua',
     'locale.lua',
     'languages/*.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server.lua'
+    'server/main.lua'
 }
-
 
 client_scripts {
-    '/client/functions.lua',
-    '/client/client.lua',
-    '/client/npcspawn.lua',
-    '/client/shopped.lua',
-    '/client/menusetup.lua',
-    '/client/searches.lua',
-    '/client/skinned.js',
-    '/client/skinned.lua'
+    'client/functions.lua',
+    'client/main.lua',
+    'client/npc_spawn.lua',
+    'client/animal_spawn.lua',
+    'client/menu.lua',
+    'client/searches.lua',
+    'client/skinned.js',
+    'client/skinned.lua'
 }
-
-version '1.2.3'
 
 dependency {
     'vorp_core',
     'vorp_inventory',
-    'vorp_utils',
     'bcc-utils',
     'feather-menu'
 }
+
+version '2.0.0'
