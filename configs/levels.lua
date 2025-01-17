42bf35023c3debea40e96ae15dccca5162b31df3
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-01-16 16:42:58 -0800

    Create levels.lua

---
+++ configs/levels.lua
@@ -0,0 +1,106 @@
+-----------------------------------------------------
+-- Hunt Leveling System
+-----------------------------------------------------
+Levels = {
+    {
+        level = 0,     -- Minimum Level in this Range
+        nextLevel = 5, -- Minimum Level in Next Range
+        discount = 0,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 5,      -- Minimum Level in this Range
+        nextLevel = 10, -- Minimum Level in Next Range
+        discount = 5,   -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 10,     -- Minimum Level in this Range
+        nextLevel = 15, -- Minimum Level in Next Range
+        discount = 10,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 15,     -- Minimum Level in this Range
+        nextLevel = 20, -- Minimum Level in Next Range
+        discount = 15,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 20,     -- Minimum Level in this Range
+        nextLevel = 25, -- Minimum Level in Next Range
+        discount = 20,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 25,     -- Minimum Level in this Range
+        nextLevel = 30, -- Minimum Level in Next Range
+        discount = 25,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 30,     -- Minimum Level in this Range
+        nextLevel = 35, -- Minimum Level in Next Range
+        discount = 30,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 35,     -- Minimum Level in this Range
+        nextLevel = 40, -- Minimum Level in Next Range
+        discount = 35,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 40,     -- Minimum Level in this Range
+        nextLevel = 45, -- Minimum Level in Next Range
+        discount = 40,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 45,     -- Minimum Level in this Range
+        nextLevel = 50, -- Minimum Level in Next Range
+        discount = 45,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 50,     -- Minimum Level in this Range
+        nextLevel = 55, -- Minimum Level in Next Range
+        discount = 50,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 55,     -- Minimum Level in this Range
+        nextLevel = 60, -- Minimum Level in Next Range
+        discount = 55,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 60,     -- Minimum Level in this Range
+        nextLevel = 65, -- Minimum Level in Next Range
+        discount = 60,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 65,     -- Minimum Level in this Range
+        nextLevel = 70, -- Minimum Level in Next Range
+        discount = 65,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 70,     -- Minimum Level in this Range
+        nextLevel = 75, -- Minimum Level in Next Range
+        discount = 70,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 75,     -- Minimum Level in this Range
+        nextLevel = 80, -- Minimum Level in Next Range
+        discount = 75,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 80,     -- Minimum Level in this Range
+        nextLevel = 85, -- Minimum Level in Next Range
+        discount = 80,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 85,     -- Minimum Level in this Range
+        nextLevel = 90, -- Minimum Level in Next Range
+        discount = 85,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 90,     -- Minimum Level in this Range
+        nextLevel = 95, -- Minimum Level in Next Range
+        discount = 90,  -- Percentage Discount on Hunt Price
+    },
+    {
+        level = 95,      -- Minimum Level in this Range
+        nextLevel = 100, -- Minimum Level in Next Range
+        discount = 95,   -- Percentage Discount on Hunt Price
+    },
+    --Can add levels by copy pasting a table and changing what you need
+}
