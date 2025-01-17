5daf2a53f0e8f5f9b143c73672ae6d5a53ce0fc7
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-01-16 16:42:23 -0800

    Update functions.lua

--- client/functions.lua
+++ client/functions.lua
@@ -1,79 +1,57 @@
---Pulling Essentials
-VORPutils = {}
-TriggerEvent("getUtils", function(utils)
-    VORPutils = utils
-end)
-VORPcore = {}
-TriggerEvent("getCore", function(core)
-    VORPcore = core
-end)
-BccUtils = exports['bcc-utils'].initiate()
-FeatherMenu =  exports['feather-menu'].initiate()
-BCCLegendarieshMenu = FeatherMenu:RegisterMenu('bcc-legendaries:Menu', {
-    top = '40%',
-    left = '20%',
-    ['720width'] = '500px',
-    ['1080width'] = '600px',
-    ['2kwidth'] = '700px',
-    ['4kwidth'] = '900px',
-    style = {},
-    contentslot = {
-        style = { --This style is what is currently making the content slot scoped and scrollable. If you delete this, it will make the content height dynamic to its inner content.
-            ['height'] = '500px',
-            ['min-height'] = '500px'
-        }
-    },
-    draggable = true
-})
+Core = exports.vorp_core:GetCore()
 
-function modelload(model) --Function To load the model
-    RequestModel(model)
+InMenu, StopAll, InMission = false, false, false
+
+function LoadModel(model, modelName)
+    if not IsModelValid(model) then
+        print('Invalid model:', modelName)
+        return
+    end
+
+    RequestModel(model, false)
+
+    local timeout = 10000
+    local startTime = GetGameTimer()
+
     while not HasModelLoaded(model) do
-      Wait(100)
-    end
-end
-
-function distcheck(x, y, z, dist, entity) --Function used to handle distance checking
-    while true do
-        if StopAll then break end
-        Wait(200)
-        local ec = GetEntityCoords(entity)
-        if GetDistanceBetweenCoords(ec.x, ec.y, ec.z, x, y, z, true) < dist then break end
-    end
-end
-
-function spawnPed(model, x, y, z, networked)
-    local createdped = CreatePed(model, x, y, z, networked, true, true, true)
-    Citizen.InvokeNative(0x283978A15512B2FE, createdped, true)
-    Citizen.InvokeNative(0x23f74c2fda6e7c61, 953018525, createdped)
-
-    return createdped
-end
-
-function SetOutfitPreset(ped,preset)
-    return Citizen.InvokeNative(0x77FF8D35EEC6BBC4,ped,preset)
-end
-
-function IsLegendaryDeadly(pedid)
-    local Predators = {
-        `mp_a_c_bear_01`,
-        `mp_a_c_wolf_01`,
-        `mp_a_c_panther_01`,
-        `mp_a_c_cougar_01`,
-        `mp_a_c_alligator_01`,
-        `a_c_alligator_02`,
-        `a_c_snake_01`,
-        `a_c_snakeredboa10ft_01`,
-		`a_c_lionmangy_01`,
-        `a_c_cougar_01`,
-        `A_C_Wolf`,
-        `a_c_bear_01`,
-    }
-    local model = GetEntityModel(pedid)
-    for i,v in pairs(Predators) do
-        if model == v then
-            return true
+        if GetGameTimer() - startTime > timeout then
+            print('Failed to load model:', modelName)
+            return
         end
+        Wait(10)
     end
-    return false
 end
+
+function StartGPS(x, y, z)
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    local color = joaat('COLOR_RED')
+
+    StartGpsMultiRoute(color, true, true)
+    AddPointToGpsMultiRoute(playerCoords.x, playerCoords.y, playerCoords.z, false)
+    AddPointToGpsMultiRoute(x, y, z, false)
+    SetGpsMultiRouteRender(true)
+end
+
+function DistanceCheck(coords, dist, entity)
+    local entityCoords = GetEntityCoords(entity)
+    local distance = #(entityCoords - coords)
+
+    while distance > dist do
+        if StopAll then
+            break
+        end
+
+        Wait(200)
+
+        entityCoords = GetEntityCoords(entity)
+        distance = #(entityCoords - coords)
+    end
+end
+
+RegisterCommand(Config.commands.level, function(source, args, rawCommand)
+    local level = Core.Callback.TriggerAwait('bcc-legendaries:GetPlayerLevel')
+    if not level then
+        level = 0
+    end
+    Core.NotifyRightTip(_U('LevelDisp') .. tostring(level), 5000)
+end, false)
